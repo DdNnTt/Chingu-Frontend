706336ceed8c60c4b2818dcb96f036fa80f936b6
sooozi <skme1103@naver.com>
2025-02-25 19:06:14 +0900

    test: commit test

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -7,9 +7,13 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 const Input: React.FC<InputProps> = ({ label, className, ...props }) => {
   return (
     <div className="mb-4">
-      <label htmlFor={label} className="block text-sm font-medium text-gray-700">
+      <label
+        htmlFor={label}
+        className="block text-sm font-medium text-gray-700"
+      >
         {label}
       </label>
+
       <input
         id={label}
         className={`mt-1 block w-full px-3 py-2 border bg-[#f3f3f5] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-300 ${className}`}
