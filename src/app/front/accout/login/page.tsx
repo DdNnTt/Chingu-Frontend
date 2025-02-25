362a3b4d70063ed584d1309bd5abbe37e437bf87
sooozi <skme1103@naver.com>
2025-02-25 18:45:49 +0900

    refactor: HTMLAttributes를 사용해 input, button props 수정

--- src/app/front/accout/login/page.tsx
+++ src/app/front/accout/login/page.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
 import React, { useState } from 'react';
-import Input from "../../../../components/common/Input";
-import Button from "../../../../components/common/Button";
+import Input from '../../../../components/common/Input';
+import Button from '../../../../components/common/Button';
 
 export default function Login() {
   const handleSubmit = (e: React.FormEvent) => {
@@ -23,17 +23,40 @@ export default function Login() {
     setPassword(e.target.value);
   };
 
-  const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleConfirmPasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setConfirmPassword(e.target.value);
   };
 
   return (
-    <div className="py-4 px-4">
-      <h2 className="text-2xl font-semibold mb-4">회원가입</h2>
+    <div className="login-page py-4 px-4">
+      <h2 className="text-2xl font-semibold mb-4">로그인</h2>
       <form onSubmit={handleSubmit}>
-        <Input label="이메일" type="email" value={email} onChange={handleEmailChange} placeholder="이메일을 입력하세요" required />
-        <Input label="비밀번호" type="password" value={password} onChange={handlePasswordChange} placeholder="비밀번호를 입력하세요" required />
-        <Input label="비밀번호 확인" type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} placeholder="비밀번호를 다시 입력하세요" required />
+        <Input
+          label="이메일"
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          placeholder="이메일을 입력하세요"
+          required
+        />
+        <Input
+          label="비밀번호"
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="비밀번호를 입력하세요"
+          required
+        />
+        <Input
+          label="비밀번호 확인"
+          type="password"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+          placeholder="비밀번호를 다시 입력하세요"
+          required
+        />
         <Button type="submit" className="w-full mt-4 bg-blue-600 text-white">
           회원가입
         </Button>
