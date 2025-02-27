ea2d547195d90b33572c25f853a0b33b32b2bcf3
henny <kimhyein1105@gmail.com>
2025-02-28 02:38:25 +0900

    chore: class명 변경

--- src/app/front/my-home/page.tsx
+++ src/app/front/my-home/page.tsx
@@ -7,7 +7,7 @@ export default function MyHome() {
     <div className="my-home-page py-4 px-4 pt-20 mx-auto rounded-lg bg-gray-100">
       <h2 className="text-2xl font-semibold mb-6 text-center">마이 홈</h2>
 
-      <div className="flex items-center mb-4 p-4 bg-white rounded-lg shadow-sm gap-2">
+      <div className="profile-card flex items-center mb-4 p-4 bg-white rounded-lg shadow-sm gap-2">
         <Image
           src="/images/test-profile.png"
           alt="프로필 사진"
@@ -15,7 +15,7 @@ export default function MyHome() {
           height={64}
           className="object-cover rounded-full border border-gray-300"
         />
-        <div className="txt-box">
+        <div className="profile-info">
           <h3 className="text-lg font-semibold">닉네임</h3>
           <p className="text-sm text-gray-500">
             친구 수 <span>20</span>
@@ -23,14 +23,14 @@ export default function MyHome() {
         </div>
       </div>
 
-      <div className="introduce p-4 bg-white rounded-lg shadow-sm mb-4">
+      <div className="profile-intro p-4 bg-white rounded-lg shadow-sm mb-4">
         <p className="text-gray-700">자기소개 멘트</p>
       </div>
 
       <div className="flex gap-2 mb-4">
         <Link
           href="/front/message/list"
-          className="flex-1 bg-main-color text-white px-4 py-3 rounded-md text-center flex items-center justify-center"
+          className="message-box flex-1 bg-main-color text-white px-4 py-3 rounded-md text-center flex items-center justify-center"
         >
           <span>나의 쪽지함</span>
           <span className="ml-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
@@ -38,10 +38,7 @@ export default function MyHome() {
           </span>
         </Link>
 
-        <Button
-          type="button"
-          className="flex-1 w-full bg-blue-600 text-white bg-gray-200"
-        >
+        <Button type="button" className="flex-1 text-white">
           일정 등록
         </Button>
       </div>
@@ -50,11 +47,11 @@ export default function MyHome() {
         일정 캘린더가 들어갈 부분
       </div>
 
-      <div className="group-list bg-white p-4 rounded-lg shadow-sm">
+      <div className="my-groups bg-white p-4 rounded-lg shadow-sm">
         <h3 className="text-lg font-semibold mb-2">내 그룹 목록</h3>
-        <div className="bg-gray-200 p-3 rounded mb-2">그룹1</div>
-        <div className="bg-gray-200 p-3 rounded mb-2">그룹2</div>
-        <div className="bg-gray-200 p-3 rounded">그룹3</div>
+        <div className="group-item bg-gray-200 p-3 rounded mb-2">그룹1</div>
+        <div className="group-item bg-gray-200 p-3 rounded mb-2">그룹2</div>
+        <div className="group-item bg-gray-200 p-3 rounded">그룹3</div>
 
         <Button
           type="button"
