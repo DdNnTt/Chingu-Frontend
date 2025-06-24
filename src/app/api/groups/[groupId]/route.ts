f3d52a15862a7202f1aedfad0f5a5e7d18d07f3b
sooozi <skme1103@naver.com>
2025-06-24 22:36:07 +0900

    fix: type error

--- src/app/api/groups/[groupId]/route.ts
+++ src/app/api/groups/[groupId]/route.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function DELETE(req: NextRequest) {
+export async function DELETE(
+  req: NextRequest,
+  context: { params: { groupId: string } }
+) {
   const token = req.headers.get('authorization');
-  const url = new URL(req.url);
-  const groupId = url.pathname.split('/').pop(); // /api/groups/[groupId] → groupId 추출
+  const groupId = context.params.groupId;
 
   if (!groupId) {
     return NextResponse.json({ message: 'groupId 누락' }, { status: 400 });
