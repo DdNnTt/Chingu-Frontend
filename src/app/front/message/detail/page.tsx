a0b00ed9627322fe037c2462457bdb476f755d76
sooozi <skme1103@naver.com>
2025-07-03 22:15:52 +0900

    feat: 팀 프로젝트 배포 시 개인 레포 자동 배포 테스트

--- src/app/front/message/detail/page.tsx
+++ src/app/front/message/detail/page.tsx
@@ -3,7 +3,7 @@ import MessageDetail from './MessageDetail';
 
 export default function MessageDetailPage() {
   return (
-    <Suspense fallback={<div>쪽지를 불러오는 중</div>}>
+    <Suspense fallback={<div>쪽지를 불러오는 중...</div>}>
       <MessageDetail />
     </Suspense>
   );
