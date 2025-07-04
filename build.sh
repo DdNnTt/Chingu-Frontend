0de1b5ae884ef4b123d56d369d756b63c35c3d9e
sooozi <skme1103@naver.com>
2025-07-04 22:16:13 +0900

    fix: build.sh 파일 수정

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 # 팀 레포에서 개인 레포로 코드 복사
 
 #!/bin/sh
-cd ../
-mkdir output
-cp -R ./Chingu-Frontend/* ./output
-cp -R ./output ./Chingu-Frontend/
 
-# Git 추적 강제용 더미 파일 생성
+mkdir -p output
+cp -R ./* ./output
+
+# .git, .github 등 불필요한 항목 제외 (선택)
+rm -rf ./output/.git
+rm -rf ./output/.github
+
+# Git 추적용 더미 파일 추가
 echo "배포용 더미 파일입니다." > ./output/.keep
 
 # 디버깅용 출력
