87a3b93c87617f5095ccbdcb471ef6fcf527d909
sooozi <skme1103@naver.com>
2025-07-04 13:26:06 +0000

    Auto Deploy from develop branch

---
+++ output/next.config.ts
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['chingu-album.s3.ap-northeast-2.amazonaws.com'],
+  },
+  async rewrites() {
+    const apiBaseUrl = process.env.API_BASE_URL;
+
+    if (!apiBaseUrl) {
+      console.warn('⚠️ API_BASE_URL is not defined');
+      return [];
+    }
+
+    return [
+      {
+        source: '/api/users/:path*',
+        destination: `${apiBaseUrl}/api/users/:path*`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
