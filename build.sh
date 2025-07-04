2123c32e7351c06c8eebbc990b401de93a4438d7
sooozi <skme1103@naver.com>
2025-07-04 22:12:27 +0900

    test: trigger deploy action

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@ cd ../
 mkdir output
 cp -R ./Chingu-Frontend/* ./output
 cp -R ./output ./Chingu-Frontend/
+
+# Git 추적 강제용 더미 파일 생성
+echo "배포용 더미 파일입니다." > ./output/.keep
+
+# 디버깅용 출력
+echo "📦 output 폴더 내용:"
+ls -al output
